9e6d497eafcb9adc7983a0254c539b7ee83f744b
dgullou <46993924+dgullou@users.noreply.github.com>
2021-11-02 21:23:35 +0800

    Update glob_function.py

--- glob_function.py
+++ glob_function.py
@@ -4,3 +4,11 @@ files = glob.glob("totalsize.py")+glob.glob("os*.py")
 
 for file in files:
     print(file)
+    
+# The result is as follow:
+# totalsize.py
+# os.path.join_basic.py
+# os_path_file_exist_remove.py
+# os_path_function.py
+# os_system_function.py
+# os_walk_basic.py
