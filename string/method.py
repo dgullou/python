d1a11cb10daeb7107a0a52f9cb41a891cee74c47
dgullou <dgullou@gmail.com>
2025-03-07 17:08:19 +0800

    Update method.py

--- string/method.py
+++ string/method.py
@@ -22,3 +22,23 @@ print("{name}, {address}, {age}".format(name="Wilson", age=25, address="Tw")) #W
 myname = "Wilson"
 age = 25
 print(f"hello, my name is {myname}, I am {age} years old.") #hello, my name is Wilson, I am 25 years old.
+
+string = "Good day is a good day."
+print(string.count("good")) #1
+print(string.lower().count("good")) #2
+#find vs index
+print(string.find("day")) #5 在index 5
+print(string.find("apple")) #-1 因為找不到
+print(string.index("day")) #5
+#print(string.index("apple")) #ValueError: substring not found
+#所以find找不到會出現-1, index找不到會出現error message
+#index shouldn't be used when you've not sure the substring can be found
+#index can be used to "string, list, temples" of python
+#find just only be used to string of python
+
+n1 = "Wilson"
+print(n1.startswith("W")) # true
+print(n1.startswith("w")) #false
+print(n1.endswith("son")) #true
+
+
---
+++ string/other_rule.py
@@ -0,0 +1,6 @@
+name = "sam Donaldson"
+name = 'P' + name[1:]
+print(name) #Pam Donaldson
+
+print("a"*5) #aaaaa
+
