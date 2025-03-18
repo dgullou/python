8d1466709fb68e558d81e572d85ce3758eae0798
dgullou <dgullou@gmail.com>
2025-03-18 19:14:10 +0800

    Dictonary

---
+++ Dictionary/basic.py
@@ -0,0 +1,13 @@
+person = {"name": "jason", "age":45}
+print(person["name"]) #jason
+print(person["age"]) #45
+
+p1 = {"x": {"age": [10,20,30]}}
+print(p1["x"]["age"][2]) #30
+
+x={}
+x["name"] = "grace"
+print(x) #{'name': 'grace'}
+x1 = {'name': 'grace', 'age':26}
+x1["name"] = 'tony'
+print(x1) #{'name': 'tony', 'age': 26}
