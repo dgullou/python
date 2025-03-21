82d96eea16b7b5ee5988087e09f6c19c0de3996b
dgullou <dgullou@gmail.com>
2025-03-21 19:10:55 +0800

    dict_methods.py

---
+++ Dictionary/dict_methods.py
@@ -0,0 +1,4 @@
+x = {'name':'Grace', 'age':26}
+print(x.keys()) #dict_keys(['name', 'age'])
+print(x.values()) #dict_values(['Grace', 26])
+print(x.items()) #dict_items([('name', 'Grace'), ('age', 26)])
