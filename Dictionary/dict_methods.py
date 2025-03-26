45ef2918077478d4bdc089af56aa0678582c08a5
dgullou <dgullou@gmail.com>
2025-03-26 15:59:25 +0800

    Tuples

--- Dictionary/dict_methods.py
+++ Dictionary/dict_methods.py
@@ -2,3 +2,5 @@ x = {'name':'Grace', 'age':26}
 print(x.keys()) #dict_keys(['name', 'age'])
 print(x.values()) #dict_values(['Grace', 26])
 print(x.items()) #dict_items([('name', 'Grace'), ('age', 26)])
+
+
---
+++ Tuples/basic.py
@@ -0,0 +1,7 @@
+mytuple = (10,"100","hello")
+print(len(mytuple)) #3
+
+print(mytuple[0]) #10
+print(mytuple[0:2]) #(10, '100')
+print(mytuple.count(10)) #1
+print(mytuple.index('hello')) #2
